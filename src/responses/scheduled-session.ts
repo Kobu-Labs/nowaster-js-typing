34c8e539806b42f4774a2e4517c4814de8e08e1d
Kobu <thekobu@gmail.com>
2023-11-09 19:42:18 +0100

    feat: add response validation for scheduled session

---
+++ src/responses/scheduled-session.ts
@@ -0,0 +1,20 @@
+import { ScheduledSessionWithIdSchema } from "@/models";
+import { z } from "zod";
+
+const create = ScheduledSessionWithIdSchema;
+const readById = z.array(ScheduledSessionWithIdSchema);
+const update = ScheduledSessionWithIdSchema;
+const remove = ScheduledSessionWithIdSchema;
+const getCategories = z.array(z.string());
+
+export type ScheduledSessionResponse = {
+    [Property in (keyof typeof ScheduledSessionResponseSchema)]: z.infer<typeof ScheduledSessionResponseSchema[Property]>
+}
+
+export const ScheduledSessionResponseSchema = {
+  create,
+  readById,
+  update,
+  remove,
+  getCategories,
+} as const;
